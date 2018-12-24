6bd6d2a4fcac42948730bc61b268bc2d83e036fb
Silvester <s.g.delarie@gmail.com>
2018-12-24 08:43:18 +0100

    for loop! <3
    Now I just need to add to the 'nameData' array, instead of overwriting it each turn of the loop.. :-/

--- frontierEosNameGenerator/src/app/random-name-selector.service.ts
+++ frontierEosNameGenerator/src/app/random-name-selector.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { InputSelectorComponent } from './input-selector/input-selector.component'
 import { NameCardComponent } from './name-card/name-card.component'
 import { generatedName } from './generatedName';
+import * as _ from 'lodash';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,34 +14,33 @@ export class RandomNameSelectorService {
   factionImageTopPath:string;
   factionImageBottomPath:string;
   nameData:generatedName[];
-  // need to declare my generatedName array here
+  counter:number;
+
   constructor() { }
   
   public getInputSelectorFormData(dataFromInputSelectorForm): void {
     this.receivedInputFormData = dataFromInputSelectorForm;
   
     console.log("We should generate " + this.receivedInputFormData.quantity + " " + this.receivedInputFormData.faction + " name.");
-    // this foreach loop doesn't work - why not? >_> even forOwn doesn't work. What the fuck?!...
-    forOwn (this.receivedInputFormData, function (value, key) {
-    /*  this.factionImageTopPath = "./assets/images/cardBackgrounds/" + this.receivedInputFormData.faction + "Top.png";
+
+    for (this.counter = 0; this.counter < this.receivedInputFormData.quantity; this.counter++ ) {
+      this.factionImageTopPath = "./assets/images/cardBackgrounds/" + this.receivedInputFormData.faction + "Top.png";
       this.factionImageBottomPath = "./assets/images/cardBackgrounds/" + this.receivedInputFormData.faction + "Bottom.png";
-      // the declare array below needs to turn into 'add this information to this array'
-        this.nameData = [
-          {
-            id:this.receivedInputFormData.quantity,
-            factionName:this.receivedInputFormData.faction,
-            factionColor:'237, 28, 36, 255',
-            factionImageTop:this.factionImageTopPath,
-            factionImageBottom:this.factionImageBottomPath,
-            name1:'Sam',
-            name2:'Porter',
-            name3:'Karter',
-            fullName:'Sam Porter Karter',
-          },
-        ]; */
-        console.log(value);
-        console.log(key);
-    });
+      // I keep overwriting my data instead of adding too it :c how do I fix this...
+      this.nameData = this.nameData.concat( [
+        {
+          id:this.receivedInputFormData.quantity,
+          factionName:this.receivedInputFormData.faction,
+          factionColor:'237, 28, 36, 255',
+          factionImageTop:this.factionImageTopPath,
+          factionImageBottom:this.factionImageBottomPath,
+          name1:'Sam',
+          name2:'Porter',
+          name3:'Karter',
+          fullName:'Sam Porter Karter',
+        },
+      ]);
+    };
     console.log(this.nameData);
   }
   
