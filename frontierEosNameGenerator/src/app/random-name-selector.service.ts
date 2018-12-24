0f6a69f6460b5e4c2435497b3759587c12cda686
Silvester <s.g.delarie@gmail.com>
2018-12-24 08:23:02 +0100

    forOwn forEach :C
    Neither seems to work, likely I am missing an import, but internet isn't telling me what I'm missing..!

--- frontierEosNameGenerator/src/app/random-name-selector.service.ts
+++ frontierEosNameGenerator/src/app/random-name-selector.service.ts
@@ -11,6 +11,7 @@ export class RandomNameSelectorService {
   receivedInputFormData: any;
   factionImageTopPath:string;
   factionImageBottomPath:string;
+  nameData:generatedName[];
   // need to declare my generatedName array here
   constructor() { }
   
@@ -18,26 +19,28 @@ export class RandomNameSelectorService {
     this.receivedInputFormData = dataFromInputSelectorForm;
   
     console.log("We should generate " + this.receivedInputFormData.quantity + " " + this.receivedInputFormData.faction + " name.");
-    // this foreach loop doesn't work - why not?
-    foreach (this.receivedInputFormData.quantity in this.receivedInputFormData) {
-      this.factionImageTopPath = "./assets/images/cardBackgrounds/" + this.receivedInputFormData.faction + "Top.png";
+    // this foreach loop doesn't work - why not? >_> even forOwn doesn't work. What the fuck?!...
+    forOwn (this.receivedInputFormData, function (value, key) {
+    /*  this.factionImageTopPath = "./assets/images/cardBackgrounds/" + this.receivedInputFormData.faction + "Top.png";
       this.factionImageBottomPath = "./assets/images/cardBackgrounds/" + this.receivedInputFormData.faction + "Bottom.png";
       // the declare array below needs to turn into 'add this information to this array'
-      let nameData: generatedName[] = [
-        {
-          id:this.receivedInputFormData.quantity,
-          factionName:this.receivedInputFormData.faction,
-          factionColor:'237, 28, 36, 255',
-          factionImageTop:this.factionImageTopPath,
-          factionImageBottom:this.factionImageBottomPath,
-          name1:'Sam',
-          name2:'Porter',
-          name3:'Karter',
-          fullName:'Sam Porter Karter',
-        },
-      ];
-    };
-    console.log(nameData);
+        this.nameData = [
+          {
+            id:this.receivedInputFormData.quantity,
+            factionName:this.receivedInputFormData.faction,
+            factionColor:'237, 28, 36, 255',
+            factionImageTop:this.factionImageTopPath,
+            factionImageBottom:this.factionImageBottomPath,
+            name1:'Sam',
+            name2:'Porter',
+            name3:'Karter',
+            fullName:'Sam Porter Karter',
+          },
+        ]; */
+        console.log(value);
+        console.log(key);
+    });
+    console.log(this.nameData);
   }
   
 }
