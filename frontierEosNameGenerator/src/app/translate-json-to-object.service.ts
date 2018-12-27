6d8a226ac6284eafad925167a66dbe7cd908f0d3
Silvester <s.g.delarie@gmail.com>
2018-12-27 14:17:18 +0100

    data subscribe
    This method should work, but I'm doing it wrong..!

--- frontierEosNameGenerator/src/app/translate-json-to-object.service.ts
+++ frontierEosNameGenerator/src/app/translate-json-to-object.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
+import 'rxjs/add/operator/map'
 import { FactionNameListClass } from './faction-name-list-class'
 
 @Injectable({
@@ -11,16 +12,20 @@ export class TranslateJsonToObjectService {
   factionNameList:FactionNameListClass;
   output:any;
   
+  
   private getNameListWithHttpRequest(location) {
     return this.http.get<FactionNameListClass>(location);
   }
   
   public readNameListFromFaction(factionName) {
-    this.output = this.getNameListWithHttpRequest('./assets/namelists/' + factionName + 'NameList.json')
-    .subscribe((data: FactionNameListClass) => this.factionNameList = { ... data });
-    console.log(this.output)
-    // getting an error when trying to log the factionNameList - not sure what's wrong - unexpected symbols?
-    console.log(this.factionNameList)
+    this.getNameListWithHttpRequest('./assets/namelists/' + factionName + 'NameList.json')
+    .map(data) => data.json as Array<Item>
+    .subscribe(data => {
+      this.factionNameList = data;
+      console.log(this.factionNameList);
+    });
+    // Internet! You're no help at all! https://stackoverflow.com/questions/44042223/load-json-from-local-file-with-http-get-in-angular-2/44042788
+    
   }
 
   constructor(private http: HttpClient) { }
